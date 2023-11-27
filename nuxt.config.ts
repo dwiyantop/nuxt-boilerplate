7b1244b4b216a35879c40f7d41c3718f119bdae9
Dwiyanto Saputra <dwiyantosaputra@NB-Dwiyanto-Tech.local>
2023-11-27 13:11:55 +0700

    feat: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: {
+    enabled: true
+  },
+  app: {
+    pageTransition: {
+      name: 'page',
+      mode: 'in-out'
+    }
+  },
+  css: [ '~/assets/css/main.css' ],
+  postcss: {
+    plugins: {
+      tailwindcss: {
+      },
+      autoprefixer: {
+      }
+    }
+  },
+  modules: [ '@nuxtjs/google-fonts' ],
+  googleFonts: {
+    download: true,
+    outputDir: 'assets',
+    overwriting: false, // set true if font families changed
+    display: 'swap',
+    families: {
+      'Plus+Jakarta+Sans': {
+        wght: [
+          300, 400, 500, 600, 700, 800
+        ]
+      }
+    }
+  }
+});
---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from 'tailwindcss';
+
+export default <Partial<Config>> {
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue'
+  ],
+  theme: {
+    fontFamily: {
+      sans: [
+        '"Plus Jakarta Sans"', 'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"'
+      ]
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio')
+  ]
+};
